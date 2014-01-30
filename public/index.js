01cc084e4724b2baa4f168d7a053ad690f8ffc2f
léo poupet <leo.poupet@gmail.com>
2014-01-30 11:08:04 +0100

    commit end ArrayToList

--- public/index.js
+++ public/index.js
@@ -1,21 +1,43 @@
+var characters = [
+    {"name": "Zombie"},
+    {"name": "Dwarf"},
+    {"name": "Troll"},
+    {"name": "Orc"},
+    {"name": "Elf"}
+];
+
 var WIAB = (function (document) {
     var WIAB = {};
 
     return WIAB;
 })(document);
 
-// data related submodule
-var WIAB.data = (function (document, WIAB) {
-    return {};
+WIAB.data = (function (document, WIAB) {
+	var getCharacters = (function () {
+		return characters;
+	});
+	return {getCharactersList : getCharacters}; // objet pour exposer notre API public
 })(document, WIAB);
 
-// view related submodule
-var WIAB.view = (function (document, WIAB) {
-    return {};
+WIAB.view = (function (document, WIAB) {
+	var renderCharacterList = (function (characters) {
+		var doc = document.getElementById("liste");
+		var ul = document.createElement('ul');
+		doc.appendChild(ul);
+		characters.forEach(function (element, index, array) {
+			var li = document.createElement('li');
+			var text = document.createTextNode(element.name);
+			ul.appendChild(li);
+			li.appendChild(text);
+		});
+	});
+    return {renderCharacterList : renderCharacterList};
 })(document, WIAB);
 
 // Boot the application
 WIAB.boot = function boot () {
+	var data = WIAB.data.getCharactersList();
+	var view = WIAB.view.renderCharacterList(data);
 };
 
 // In a jQuery world, you would use `$(function(){...})`
