973a62de1855f13acfe5885bd159b060affba728
léo poupet <leo.poupet@gmail.com>
2014-01-30 12:00:34 +0100

    commit changing bar-mood in progress

--- public/index.js
+++ public/index.js
@@ -16,20 +16,36 @@ WIAB.data = (function (document, WIAB) {
 	var getCharacters = (function () {
 		return characters;
 	});
-	return {getCharactersList : getCharacters}; // objet pour exposer notre API public
+	return {getCharactersList : getCharacters};
 })(document, WIAB);
 
 WIAB.view = (function (document, WIAB) {
 	var renderCharacterList = (function (characters) {
-		var doc = document.getElementById("liste");
-		var ul = document.createElement('ul');
-		doc.appendChild(ul);
+		var ul = document.getElementById('character-list');
 		characters.forEach(function (element, index, array) {
 			var li = document.createElement('li');
 			var text = document.createTextNode(element.name);
+			var input = document.createElement('input');
+			var label = document.createElement('label');
+			input.setAttribute('type','checkbox');
+			input.setAttribute('name','character-id');
+			input.setAttribute('value','10');
+			input.setAttribute('id','character-id');
+			label.setAttribute('for','character-id');
+			label.appendChild(text);
 			ul.appendChild(li);
-			li.appendChild(text);
+			li.appendChild(input);
+			li.appendChild(label);
+			attachListEvents(input);
 		});
+	});
+	
+	var attachListEvents = (function attachListEvents(element){
+		element.addEventListener('click',function(){
+			var bar = document.getElementsByClassName('bar-mood')[0];
+			console.log(bar);
+			bar.innerHTML = "Coucou";
+			});
 	});
     return {renderCharacterList : renderCharacterList};
 })(document, WIAB);
